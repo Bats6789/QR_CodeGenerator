043cff59eaf2af0723163843f6c41b151dab09a7
Blake Wingard <bwingard@IBML.local>
2025-02-20 14:27:05 -0600

    Fixed format and started Reed Solomon

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <inttypes.h>
 #include <math.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "image_export.h"
 
@@ -17,132 +19,162 @@
 #define FORMAT_XOR_MASK 0b101010000010010
 
 uint16_t BCH(uint16_t data) {
-  uint16_t res = 0;
-  size_t bit_len = floor(log2(data)) + 1;
-  uint16_t poly = 0b0100110111;
+    uint16_t res = 0;
+    size_t bit_len = floor(log2(data)) + 1;
+    uint16_t poly = 0b0100110111;
 
-  res = data << (10 - bit_len);
+    res = data << (10 - bit_len);
 
-  for (size_t i = 0; i < bit_len; ++i) {
-    if ((res & 0x200) == 0x200) {
-      res <<= 1;
-      res ^= poly;
-    } else {
-      res <<= 1;
+    for (size_t i = 0; i < bit_len; ++i) {
+        if ((res & 0x200) == 0x200) {
+            res <<= 1;
+            res ^= poly;
+        } else {
+            res <<= 1;
+        }
+        res &= 0x3FF;
     }
-    res &= 0x3FF;
-  }
 
-  return res;
+    return res;
+}
+
+int gf_log[256];
+int gf_exp[512];
+
+void calculate_table(void) {
+    int x = 1;
+    size_t i;
+    for (i = 0; i < 255; ++i) {
+        gf_exp[i] = x;
+        gf_log[x] = i;
+
+        if ((x & 0x80) == 0x80) {
+            x <<= 1;
+            x ^= 0x11D;
+            x &= 0xFF;
+        } else {
+            x <<= 1;
+        }
+    }
+
+    for (; i < 512; ++i) {
+        gf_exp[i] = gf_exp[i - 255];
+    }
+}
+
+size_t reed_solomon(size_t data_sz, int data[data_sz], int **error_correction) {
+    size_t ec_sz = 10;
+    int *ec = malloc(sizeof *ec * ec_sz);
+    int *buf = calloc(data_sz + ec_sz, sizeof *buf);
+    int gp[] = {1, 216, 194, 159, 111, 199, 94, 95, 113, 157, 193};
+
+    memcpy(buf, data, sizeof *data * data_sz);
+
+    for (size_t i = 0; i < data_sz; ++i) {
+        int coef = buf[i];
+        for (size_t j = 1; j < 11; ++j) {
+            buf[i + j] ^= gf_exp[gf_log[gp[j]] + gf_log[coef]];
+        }
+    }
+
+    memcpy(ec, buf + data_sz, sizeof *ec * ec_sz);
+    *error_correction = ec;
+
+    return ec_sz;
 }
 
 int main(int argc, char **argv) {
-  printf("Hello World\n");
-  size_t width = QR_SZ * PIXEL_PER_MODULE;
-  size_t height = QR_SZ * PIXEL_PER_MODULE;
-  image_t image;
-  pixel_t pixel = WHITE;
+    printf("Hello World: %d\n", 0b10);
+    size_t width = QR_SZ * PIXEL_PER_MODULE;
+    size_t height = QR_SZ * PIXEL_PER_MODULE;
+    image_t image;
+    pixel_t pixel = WHITE;
+    int *ec;
+    size_t ec_sz;
+    int data[] = {16, 32, 12, 86, 97, 128, 236, 17, 236, 17, 236, 17, 236, 17, 236, 17};
+    calculate_table();
+    ec_sz = reed_solomon(16, data, &ec);
 
-  uint16_t format = ((FORMAT_VAL << 10) + BCH(FORMAT_VAL)) ^ FORMAT_XOR_MASK;
-  printf("0b%015B\n", format);
-
-  image = init_image(width, height);
-
-  for (size_t i = 0; i < QR_SZ; ++i) {
-    for (size_t j = 0; j < QR_SZ; ++j) {
-      // assume white and repaint if not
-      pixel = WHITE;
-      size_t mod_row = i - 4;
-      size_t mod_col = j - 4;
-
-      // finder pattern and timing pattern (Same properties for every QR code)
-      if ((4 <= i && mod_row < MODULE_SZ && 4 <= j && mod_col < MODULE_SZ) &&
-          (((mod_row == 0 || mod_row == 6) &&
-            (mod_col < 7 || mod_col >= MODULE_SZ - 7)) ||
-           ((mod_row == MODULE_SZ - 7 || mod_row == MODULE_SZ - 1) &&
-            mod_col < 7) ||
-           ((mod_row < 7 || mod_row >= MODULE_SZ - 7) &&
-            (mod_col == 0 || mod_col == 6)) ||
-           (mod_row < 7 &&
-            (mod_col == MODULE_SZ - 7 || mod_col == MODULE_SZ - 1)) ||
-           ((2 <= mod_row && mod_row <= 4) &&
-            ((2 <= mod_col && mod_col <= 4) ||
-             (MODULE_SZ - 5 <= mod_col && mod_col <= MODULE_SZ - 3))) ||
-           ((MODULE_SZ - 5 <= mod_row && mod_row <= MODULE_SZ - 3) &&
-            (2 <= mod_col && mod_col <= 4)) ||
-           (mod_row == 6 && (7 < mod_col && mod_col < MODULE_SZ - 8) &&
-            (mod_col - 8) % 2 == 0) ||
-           (mod_col == 6 && (7 < mod_row && mod_row < MODULE_SZ - 8) &&
-            (mod_row - 8) % 2 == 0))) {
-        pixel = BLACK;
-      }
-
-      // format
-      if ((mod_col == 8 && mod_row == 4 * QR_VERSION + 9) || // Dark Module
-          ((format & 0x1) == 0x1 &&
-           ((mod_row == 0 && mod_col == 8) ||
-            (mod_row == 8 && mod_col == MODULE_SZ - 1))) || // 0
-          ((format & 0x2) == 0x2 &&
-           ((mod_row == 1 && mod_col == 8) ||
-            (mod_row == 8 && mod_col == MODULE_SZ - 2))) || // 1
-          ((format & 0x4) == 0x4 &&
-           ((mod_row == 2 && mod_col == 8) ||
-            (mod_row == 8 && mod_col == MODULE_SZ - 3))) || // 2
-          ((format & 0x8) == 0x8 &&
-           ((mod_row == 3 && mod_col == 8) ||
-            (mod_row == 8 && mod_col == MODULE_SZ - 4))) || // 3
-          ((format & 0x10) == 0x10 &&
-           ((mod_row == 4 && mod_col == 8) ||
-            (mod_row == 8 && mod_col == MODULE_SZ - 5))) || // 4
-          ((format & 0x20) == 0x20 &&
-           ((mod_row == 5 && mod_col == 8) ||
-            (mod_row == 8 && mod_col == MODULE_SZ - 6))) || // 5
-          ((format & 0x40) == 0x40 &&
-           ((mod_row == 7 && mod_col == 8) ||
-            (mod_row == 8 && mod_col == MODULE_SZ - 7))) || // 6
-          ((format & 0x80) == 0x80 &&
-           ((mod_row == 8 && mod_col == 8) ||
-            (mod_row == 8 && mod_col == MODULE_SZ - 8))) || // 7
-          ((format & 0x100) == 0x100 &&
-           ((mod_row == 8 && mod_col == 7) ||
-            (mod_row == MODULE_SZ - 7 && mod_col == 8))) || // 8
-          ((format & 0x200) == 0x200 &&
-           ((mod_row == 8 && mod_col == 5) ||
-            (mod_row == MODULE_SZ - 6 && mod_col == 8))) || // 9
-          ((format & 0x400) == 0x400 &&
-           ((mod_row == 8 && mod_col == 4) ||
-            (mod_row == MODULE_SZ - 5 && mod_col == 8))) || // 10
-          ((format & 0x800) == 0x800 &&
-           ((mod_row == 8 && mod_col == 3) ||
-            (mod_row == MODULE_SZ - 4 && mod_col == 8))) || // 11
-          ((format & 0x1000) == 0x1000 &&
-           ((mod_row == 8 && mod_col == 2) ||
-            (mod_row == MODULE_SZ - 3 && mod_col == 8))) || // 12
-          ((format & 0x2000) == 0x2000 &&
-           ((mod_row == 8 && mod_col == 1) ||
-            (mod_row == MODULE_SZ - 2 && mod_col == 8))) || // 13
-          ((format & 0x4000) == 0x4000 &&
-           ((mod_row == 8 && mod_col == 0) ||
-            (mod_row == MODULE_SZ - 1 && mod_col == 8)))) { // 14
-        pixel = BLACK;
-      }
-
-      for (size_t row = i * PIXEL_PER_MODULE; row < (i + 1) * PIXEL_PER_MODULE;
-           ++row) {
-        for (size_t col = j * PIXEL_PER_MODULE;
-             col < (j + 1) * PIXEL_PER_MODULE; ++col) {
-          image.pixels[row * width + col] = pixel;
-        }
-      }
+    for (size_t i = 0; i < ec_sz; ++i) {
+        printf("%d ", ec[i]);
     }
-  }
+    putchar('\n');
 
-  if (export_to_png("test.png", image) != 0) {
-    perror("export");
-    return EXIT_FAILURE;
-  }
+    uint16_t format = ((FORMAT_VAL << 10) + BCH(FORMAT_VAL)) ^ FORMAT_XOR_MASK;
+    printf("0b%015B\n", format);
 
-  free_image(image);
+    image = init_image(width, height);
 
-  return 0;
+    for (size_t i = 0; i < QR_SZ; ++i) {
+        for (size_t j = 0; j < QR_SZ; ++j) {
+            // assume white and repaint if not
+            pixel = WHITE;
+            size_t mod_row = i - 4;
+            size_t mod_col = j - 4;
+
+            // finder pattern and timing pattern (Same properties for every QR code)
+            if ((4 <= i && mod_row < MODULE_SZ && 4 <= j && mod_col < MODULE_SZ) &&
+                (((mod_row == 0 || mod_row == 6) && (mod_col < 7 || mod_col >= MODULE_SZ - 7)) ||
+                 ((mod_row == MODULE_SZ - 7 || mod_row == MODULE_SZ - 1) && mod_col < 7) ||
+                 ((mod_row < 7 || mod_row >= MODULE_SZ - 7) && (mod_col == 0 || mod_col == 6)) ||
+                 (mod_row < 7 && (mod_col == MODULE_SZ - 7 || mod_col == MODULE_SZ - 1)) ||
+                 ((2 <= mod_row && mod_row <= 4) &&
+                  ((2 <= mod_col && mod_col <= 4) || (MODULE_SZ - 5 <= mod_col && mod_col <= MODULE_SZ - 3))) ||
+                 ((MODULE_SZ - 5 <= mod_row && mod_row <= MODULE_SZ - 3) && (2 <= mod_col && mod_col <= 4)) ||
+                 (mod_row == 6 && (7 < mod_col && mod_col < MODULE_SZ - 8) && (mod_col - 8) % 2 == 0) ||
+                 (mod_col == 6 && (7 < mod_row && mod_row < MODULE_SZ - 8) && (mod_row - 8) % 2 == 0))) {
+                pixel = BLACK;
+            }
+
+            // format
+            if ((mod_col == 8 && mod_row == 4 * QR_VERSION + 9) ||  // Dark Module
+                ((format & 0x1) == 0x1 &&
+                 ((mod_row == 0 && mod_col == 8) || (mod_row == 8 && mod_col == MODULE_SZ - 1))) ||  // 0
+                ((format & 0x2) == 0x2 &&
+                 ((mod_row == 1 && mod_col == 8) || (mod_row == 8 && mod_col == MODULE_SZ - 2))) ||  // 1
+                ((format & 0x4) == 0x4 &&
+                 ((mod_row == 2 && mod_col == 8) || (mod_row == 8 && mod_col == MODULE_SZ - 3))) ||  // 2
+                ((format & 0x8) == 0x8 &&
+                 ((mod_row == 3 && mod_col == 8) || (mod_row == 8 && mod_col == MODULE_SZ - 4))) ||  // 3
+                ((format & 0x10) == 0x10 &&
+                 ((mod_row == 4 && mod_col == 8) || (mod_row == 8 && mod_col == MODULE_SZ - 5))) ||  // 4
+                ((format & 0x20) == 0x20 &&
+                 ((mod_row == 5 && mod_col == 8) || (mod_row == 8 && mod_col == MODULE_SZ - 6))) ||  // 5
+                ((format & 0x40) == 0x40 &&
+                 ((mod_row == 7 && mod_col == 8) || (mod_row == 8 && mod_col == MODULE_SZ - 7))) ||  // 6
+                ((format & 0x80) == 0x80 &&
+                 ((mod_row == 8 && mod_col == 8) || (mod_row == 8 && mod_col == MODULE_SZ - 8))) ||  // 7
+                ((format & 0x100) == 0x100 &&
+                 ((mod_row == 8 && mod_col == 7) || (mod_row == MODULE_SZ - 7 && mod_col == 8))) ||  // 8
+                ((format & 0x200) == 0x200 &&
+                 ((mod_row == 8 && mod_col == 5) || (mod_row == MODULE_SZ - 6 && mod_col == 8))) ||  // 9
+                ((format & 0x400) == 0x400 &&
+                 ((mod_row == 8 && mod_col == 4) || (mod_row == MODULE_SZ - 5 && mod_col == 8))) ||  // 10
+                ((format & 0x800) == 0x800 &&
+                 ((mod_row == 8 && mod_col == 3) || (mod_row == MODULE_SZ - 4 && mod_col == 8))) ||  // 11
+                ((format & 0x1000) == 0x1000 &&
+                 ((mod_row == 8 && mod_col == 2) || (mod_row == MODULE_SZ - 3 && mod_col == 8))) ||  // 12
+                ((format & 0x2000) == 0x2000 &&
+                 ((mod_row == 8 && mod_col == 1) || (mod_row == MODULE_SZ - 2 && mod_col == 8))) ||  // 13
+                ((format & 0x4000) == 0x4000 &&
+                 ((mod_row == 8 && mod_col == 0) || (mod_row == MODULE_SZ - 1 && mod_col == 8)))) {  // 14
+                pixel = BLACK;
+            }
+
+            for (size_t row = i * PIXEL_PER_MODULE; row < (i + 1) * PIXEL_PER_MODULE; ++row) {
+                for (size_t col = j * PIXEL_PER_MODULE; col < (j + 1) * PIXEL_PER_MODULE; ++col) {
+                    image.pixels[row * width + col] = pixel;
+                }
+            }
+        }
+    }
+
+    if (export_to_png("test.png", image) != 0) {
+        perror("export");
+        return EXIT_FAILURE;
+    }
+
+    free_image(image);
+
+    return 0;
 }
